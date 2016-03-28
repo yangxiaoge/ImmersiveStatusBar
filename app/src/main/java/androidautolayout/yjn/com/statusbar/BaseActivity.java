fab8e1623eb1cd8715f617d16a6f8017ce4dc8c4
yang.jianan <364804051@qq.com>
2016-03-28 10:12:02 +0800

    沉浸式状态栏!!

---
+++ app/src/main/java/androidautolayout/yjn/com/statusbar/BaseActivity.java
@@ -0,0 +1,38 @@
+package androidautolayout.yjn.com.statusbar;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.colorPrimary);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+}
---
+++ app/src/main/java/androidautolayout/yjn/com/statusbar/MainActivity.java
@@ -0,0 +1,13 @@
+package androidautolayout.yjn.com.statusbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
